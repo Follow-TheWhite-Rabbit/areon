ebf1b60530a9e36a90d53929e7cbb60fc9b4783e
Sayonara <alekss3228@gmail.com>
2021-06-27 16:23:03 +0300

    classwork03-js

---
+++ classwork/JS/work03/script.js
@@ -0,0 +1,107 @@
+// const userName = prompt("Введите имя пользователя");
+//
+// switch (userName)  {
+//     case "Mike":
+//         alert("CEO");
+//         console.log("Привет $(userName) - $(pos)");
+//         break;
+//     case "Jane":
+//         alert("CTO");
+//         console.log("Привет $(userName) - $(pos)");
+//         break;
+//     case "Walter":
+//         alert("Программист");
+//         console.log("Привет $(userName) - $(pos)");
+//         break;
+//     case "Oliver":
+//         alert("Менеджер");
+//         console.log("Привет $(userName) - $(pos)");
+//         break;
+//     case "John":
+//         alert("Уборщик");
+//         console.log("Привет $(userName) - $(pos)");
+//         break;
+//     default:
+//         pos = ("Пользователь не найден");
+//         console.log(pos);
+// }
+//
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+// const userName= prompt("Enter Name");
+//
+// if (userName === "Mike") {
+//     console.log("Hello, CEO," + userName);
+// }
+// else if (userName === "Jane") {
+//     console.log("Hello, CTO," + userName);
+// }
+// else if (userName === "Walter") {
+//     console.log("Hello, Programmer," + userName);
+// }
+// else if (userName === "Oliver") {
+//     console.log("Hello, Manager," + userName);
+// }
+// else if (userName === "John") {
+//     console.log("Hello, Cleaner," + userName);
+// }
+// else {
+//     console.log("User Unknown");
+// }
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+// let myNumber=+prompt("Введите число");
+//
+// if (myNumber % 2 === 0) {
+//     console.log("Ваше число чётное.");
+// }
+// else if (myNumber % 2 === 1) {
+//     console.log("Ваше число не чётное");
+// }
+// else {
+//     myNumber=+prompt("Необходимо ввести число!")
+//         if (isNaN(myNumber)) {
+//             console.log("Ошибка! Вы ввели не правильное число.")
+//         }
+// }
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+//
+// for (let i = 0; i <=300; i++) {
+//     if (i%2!==0){
+//         if (i%5!==0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//
+// let first;
+// let second;
+// do {
+//     first = +prompt("Enter the first number");
+// } while (isNaN(first));
+// do {
+//     second = +prompt("Enter the second number");
+// } while (isNaN(second));
+// alert(`Вітаю! Ведені вами числа: ${first} та ${second}.`);
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// let userName = prompt("Enter your name");
+// let birthDate = +prompt("Enter your year of birth");
