90b2bd95d09a9701362d88dd6d92ac47177af7b2
Sayonara <alekss3228@gmail.com>
2021-07-09 13:06:31 +0300

    js02-fix

--- basic-javascript/homework02/js/script.js
+++ basic-javascript/homework02/js/script.js
@@ -2,7 +2,7 @@ let number = +prompt("Enter the number");
     if (number < 5) {
         console.log("Sorry, no numbers");
 } else for (let i = 0; i <= number; i++) {
-            if (i % 5 === 0 && i !== 0) {
+            if (i % 5 === 0) {
                 console.log(i);
     }
 }
