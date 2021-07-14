767b58257e1ae1211b66b58e54d7ba1591d72ffe
Sayonara <alekss3228@gmail.com>
2021-07-14 17:53:55 +0300

    homework05-js

---
+++ basic-javascript/homework05/js/script.js
@@ -0,0 +1,23 @@
+function createNewUser() {
+    return {
+        userName: prompt("Введите имя"),
+        userSurname: prompt("Введите фамилию"),
+        birthday: new Date(prompt("Введите дату рождения в формате dd.mm.yyyy")),
+        getLogin: function () {
+            return (this.userName[0] + this.userSurname).toLowerCase();
+        },
+        getAge: function () {
+            const now = new Date();
+            const date = now.getFullYear() - this.birthday.getFullYear();
+            const age = new Date(date);
+            return this.birthday.getFullYear() - age.getFullYear();
+        },
+        getPassword: function () {
+            return (this.userName[0] + (this.userSurname.toLowerCase()) + this.birthday.getFullYear());
+        }
+    }
+}
+const newUser = createNewUser();
+console.log(newUser.getLogin());
+console.log(newUser.getAge());
+console.log(newUser.getPassword());
