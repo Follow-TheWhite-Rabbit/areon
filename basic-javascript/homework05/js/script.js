729f17bf5eec7822192afd0a3cf02cbb696f6e20
Sayonara <alekss3228@gmail.com>
2021-07-15 03:44:20 +0300

    HW05-JS-FIX2.0

--- basic-javascript/homework05/js/script.js
+++ basic-javascript/homework05/js/script.js
@@ -1,18 +1,52 @@
+// function createNewUser() {
+//     return {
+//         userName: prompt("Введите имя"),
+//         userSurname: prompt("Введите фамилию"),
+//         birthday: new Date(prompt("Введите дату рождения в формате dd.mm.yyyy")),
+//         getLogin: function () {
+//             return (this.userName[0] + this.userSurname).toLowerCase();
+//         },
+//         getAge: function () {
+//             const now = new Date();
+//             const date = now.getFullYear() - this.birthday.getFullYear();
+//             return date;
+//         },
+//         getPassword: function () {
+//             return (this.userName[0] + (this.userSurname.toLowerCase()) + this.birthday.getFullYear());
+//         }
+//     }
+// }
+// const newUser = createNewUser();
+// console.log(newUser.getLogin());
+// console.log(newUser.getAge());
+// console.log(newUser.getPassword());
+
+
+
+
+
+//////////// 2 ВАРИАНТ (БОЛЕЕ ПРАВИЛЬНЫЙ) ////////////////////////////////////////////////////////////////////
+
+
+
 function createNewUser() {
     return {
         userName: prompt("Введите имя"),
         userSurname: prompt("Введите фамилию"),
-        birthday: new Date(prompt("Введите дату рождения в формате dd.mm.yyyy")),
+        birthday: prompt("Введите дату рождения в формате dd.mm.yyyy"),
         getLogin: function () {
             return (this.userName[0] + this.userSurname).toLowerCase();
         },
         getAge: function () {
             const now = new Date();
-            const date = now.getFullYear() - this.birthday.getFullYear();
-            return date;
+            const birthdayDate = this.birthday.split('.');
+            const date = new Date(birthdayDate[2], birthdayDate[1] - 1, birthdayDate[0]);
+            const time = now - date;
+            const dateTime = new Date(time);
+            return new Date(Date.now() - date).getFullYear() - 1970;
         },
         getPassword: function () {
-            return (this.userName[0] + (this.userSurname.toLowerCase()) + this.birthday.getFullYear());
+            return (this.userName[0] + (this.userSurname.toLowerCase()) + this.birthday.split('.')[2]);
         }
     }
 }
