66905c194f6b7102907eb74deaeaf562f5dce11e
Sayonara <alekss3228@gmail.com>
2021-07-28 23:15:22 +0300

    hw07-js

---
+++ basic-javascript/homework07/js/script.js
@@ -0,0 +1,4 @@
+
+
+const arr = ['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Lviv'];
+document.body. insertAdjacentHTML('afterbegin', `<ul>${arr.map(town => `<li>${town}</li>`).join('')}</ul>`);
