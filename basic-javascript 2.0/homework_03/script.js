1a000153ccbc20cad1144afcb87342cb3ca11bb8
Sayonara <alekss3228@gmail.com>
2022-08-09 15:25:25 +0300

    homework03_js2.0

---
+++ basic-javascript 2.0/homework_03/script.js
@@ -0,0 +1,10 @@
+
+let number = +prompt('Enter the number')
+
+for (let i = 0; i <= number; i++) {
+    if (i % 5 === 0) {
+        console.log(i)
+    }
+} if (number < 5) {
+    alert('Sorry, no numbers')
+}
