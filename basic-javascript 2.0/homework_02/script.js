0ab8b3b11067f6ca28b10841a837a6aab7cc28ff
Sayonara <alekss3228@gmail.com>
2022-08-10 14:48:10 +0300

    homework02_js2.0_corrected

--- basic-javascript 2.0/homework_02/script.js
+++ basic-javascript 2.0/homework_02/script.js
@@ -14,8 +14,8 @@ while (isNaN(+userAge) || userAge === null || userAge === "") {
 if (userAge < 18) {
     alert('You are not allowed to visit this website')
 } else if (userAge >= 18 && userAge <= 22) {
-    confirm('Are you sure you want to continue?')
-    if (true) {
+    let ok = confirm('Are you sure you want to continue?')
+    if (ok) {
         alert(`Welcome, ${userName}`)
     } else {
         alert('You are not allowed to visit this website')
