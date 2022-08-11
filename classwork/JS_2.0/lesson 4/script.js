8595324452dae783f4ed205fdc5a35d379a8f3b7
Sayonara <alekss3228@gmail.com>
2022-08-12 00:12:46 +0300

    oi90i90i90ii

---
+++ classwork/JS_2.0/lesson 4/script.js
@@ -0,0 +1,17 @@
+
+// //Рекурсия
+//
+// function pow(x, n) {
+//     let result = 1;
+//
+//     // множимо result на x n разів в циклі
+//     for (let i = 0; i < n; i++) {
+//         result *= x;
+//     }
+//
+//     return result;
+// }
+// alert( pow(5, 4) );
+
+
+
