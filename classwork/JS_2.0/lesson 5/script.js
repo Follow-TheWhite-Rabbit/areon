16d8ec681f65c154acb261084c8de0bfb0962be7
Sayonara <alekss3228@gmail.com>
2022-08-26 01:22:13 +0300

    classwork_07(js2.0)

--- classwork/JS_2.0/lesson 5/script.js
+++ classwork/JS_2.0/lesson 5/script.js
@@ -112,6 +112,180 @@
 
 
 
+/**
+ * Задача 2.
+ *
+ * Написать мульти-язычную программу-органайзер.
+ *
+ * Программа спрашивает у пользователя на каком языке он желает увидеть список дней недели.
+ * После ввода пользователем желаемого языка программа выводит в консоль дни недели на указанном языке.
+ *
+ * Доступные языки (локали): ua, ru, en.
+ *
+ * Если введённая пользователем локаль не совпадает с доступными — программа переспрашивает его до тех пор,
+ * пока доступная локаль не будет введена.
+ *
+ * Условия:
+ * - Решение должно быть коротким, лаконичным и универсальным;
+ * - Всячески использовать методы массива;
+ * - Использование метода Object.keys() обязательно.
+ */
+
+
+
+
+// const days = {
+//     ua: [
+//         'Понеділок',
+//         'Вівторок',
+//         'Середа',
+//         'Четвер',
+//         'П’ятниця',
+//         'Субота',
+//         'Неділя',
+//     ],
+//     ru: [
+//         'Понедельник',
+//         'Вторник',
+//         'Среда',
+//         'Четверг',
+//         'Пятница',
+//         'Суббота',
+//         'Воскресенье',
+//     ],
+//     en: [
+//         'Monday',
+//         'Tuesday',
+//         'Wednesday',
+//         'Thursday',
+//         'Friday',
+//         'Saturday',
+//         'Sunday',
+//     ],
+// };
+//
+// function getLanguadge(){
+//     let userChoice = prompt("Which language do you want to have?");
+//     while (!Object.keys(days).includes(userChoice)){
+//         userChoice = prompt("Which language do you want to have?");
+//     }
+//     days[userChoice].forEach(item => console.log(item))
+// }
+//
+// console.log(Object.keys(days))
+// getLanguadge();
+
+
+/**
+ * Задача 3.
+ *
+ * Напишите функцию mergeArrays для объединения нескольких массивов в один.
+ *
+ * Функция обладает неограниченным количеством параметров.
+ * Функция возвращает один массив, который является сборным из массивов,
+ * переданных функции в качестве аргументов при её вызове.
+ *
+ * Условия:
+ * - Все аргументы функции должны обладать типом «массив», иначе генерировать ошибку;
+ * - В ошибке обязательно указать какой по счёту аргумент провоцирует ошибку.
+ *
+ * Заметки:
+ * - Делать поддержку выравнивания вложенных массивов не нужно.
+ */
+
+
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [1, 2, 3];
+// const arr3 = [1, 2, 3];
+//
+//
+// function unitedArrays(...args) {
+//     args.forEach((item, index) => {
+//         if (typeof item !== "object") {
+//             throw new Error(`Возникла ошибка в элементе номер : ${index}`);
+//         }
+//     });
+//     return new Array().concat(...args);
+// }
+// console.log(unitedArrays(arr1, arr2, arr3, "adadad"));
+
+
+//////////////// matrix //////////////////////////////////////
+
+// const matrix = [[1, 1, 1, 0],
+//     [0, 5, 0, 1],
+//     [2, 1, 3, 10]];
+//
+//
+//
+// function countMatrix(array){
+//     // let numIndex = array[1][1];
+//     // console.log(numIndex);
+//     let count = 0;
+//
+//     for(let i = 0; i < array[0].length; i++){
+//         for(let k = 0; k < array.length; k++){
+//             if(array[k][i] == 0){
+//                 break;
+//             }else{
+//                 count += array[k][i];
+//             }
+//         }
+//     }
+//     return count
+// }
+//
+// console.log(countMatrix(matrix2));
+
+
+
+
+const arr = ["aa","aba", "aa", "ad", "vcd", "aba", "vcd", "aa", "ad", "aba", "vcd", "aba"]
+
+let res = []                                     // СОЗДАЛИ НОВЫЙ МАССИВ
+
+res = arr.map((value) => value.length)    // ПЕРЕЗАПИСАЛИ В НЕГО ДЛИННУ КАЖДОЙ СТРОКИ
+
+let result = []                                 // СОЗДАЛИ ЕЩЕ ОДИН ПУСТОЙ МАССИВ
+
+for (let i = 0; i < res.length; i++) {          // С ПОМОЩЬЮ ЦИКЛА, ПЕРЕБРАЛИ МАССИВ res, И ЗАПИСАЛИ В result ИНДЕКСЫ ВСЕХ МАКСИМАЛЬНЫХ ЧИСЕЛ (В КАЧЕСТВЕ ЗНАЧЕНИЙ)
+    if (res[i] === Math.max(...res)) {
+        result.push([i])
+    }
+}
+
+let newArr = []                // СОЗДАЛИ ЕЩЕ ОДИН ПУСТОЙ МАССИВ
+
+result.forEach((value) => {    // ПЕРЕБРАЛИ МАССИВ result С ПОМОЩЬЮ МЕТОДА forEach И ЗАПИСАЛИ В НОВЫЙ МАССИВ newArr ВСЕ ДЛИННЫЕ СЛОВА, ИСПОЛЬЗУЯ ИНДЕКСЫ МАКСИМАЛЬНЫХ ЧИСЕЛ
+    newArr.push(arr[value])
+})
+
+
+console.log(res)
+console.log(result)
+console.log(newArr)
+
+
+
+
+
+// res = arr.map((value) => value.length)
+//
+// res.sort((a, b) => b-a)
+//
+// let result = 0
+//
+// for (let i = 0; i < res.length; i++) {
+//     if (res[0] === res[i]) {
+//         result++
+//     }
+// }
+//
+// console.log(res)
+// console.log(result)
+
+
 
 
 
