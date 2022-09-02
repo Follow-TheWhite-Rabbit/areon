a692571d96210f00de16827cd08135e9b5748be4
Sayonara <alekss3228@gmail.com>
2022-09-02 20:28:36 +0300

    homework09_recurcion

--- basic-javascript 2.0/homework_09/script.js
+++ basic-javascript 2.0/homework_09/script.js
@@ -1,11 +1,25 @@
 
-let arr = ["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"]
+let arr = ["Kharkiv", "Kiev", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper"]
 
 const createElement = (arr, parent = document.body) => {
     const element = document.createElement('ol')
-    element.innerHTML = arr.map(el => `<li>${el}</li>`).join(' ')
+    const filter = (arr) => arr.reduce((start, curr) => start.concat(Array.isArray(curr) ? filter(curr) : `<li>${curr}</li>`), []).join(' ')
+    element.innerHTML = filter(arr)
     parent.append(element)
 }
 
 createElement(arr)
+
+
+
+// let arr = ["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"]
+//
+// const createElement = (arr, parent = document.body) => {
+//     const element = document.createElement('ol')
+//     element.innerHTML = arr.map(el => `<li>${el}</li>`).join(' ')
+//     parent.append(element)
+// }
+//
+// createElement(arr)
+
 
