23d2ccdc0661a3ed5a26ffca398e4f05c340e0ef
Sayonara <alekss3228@gmail.com>
2022-09-03 02:19:39 +0300

    homework_practice

--- classwork/JS_2.0/practice/script.js
+++ classwork/JS_2.0/practice/script.js
@@ -1,17 +1,37 @@
 
 
-const dataIcecream = [
-    { name: "хрещатик", price: "7,99 uah" },
-    { name: "100% пломбір", price: "2,50 uah" },
-    { name: "maximus", price: "12,99 uah" },
-    { name: "пташине молоко", price: "20,99 uah" },
-]
+// const dataIcecream = [
+//     { name: "хрещатик", price: "7,99 uah" },
+//     { name: "100% пломбір", price: "2,50 uah" },
+//     { name: "maximus", price: "12,99 uah" },
+//     { name: "пташине молоко", price: "20,99 uah" },
+// ]
+//
+// const renderList = (list) => {
+//     const element = document.createElement('ul')
+//     list.forEach(value => element.innerHTML += `<li><h2>${value.name}</h2><p>${value.price}</p></li>`)
+//     document.body.append(element)
+// }
+//
+// renderList(dataIcecream)
+//
 
-const renderList = (list) => {
-    const element = document.createElement('ul')
-    list.forEach(value => element.innerHTML += `<li><h2>${value.name}</h2><p>${value.price}</p></li>`)
-    document.body.append(element)
+
+
+
+let arr = ["Kharkiv", "Kiev", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper", ["Borispol", "Irpin"]]
+
+let gogi = []
+
+function filter(el) {
+    debugger
+    if (Array.isArray(el)) {
+        el.map(filter)
+    } else {
+        return gogi.push(el)
+    }
 }
 
-renderList(dataIcecream)
+arr.map(filter)
 
+console.log(gogi)
